770fc7a3f9b73cd899756da2124522dd8e557e81
PT. Wavin Tunas Utama <ICT@PTs-MacBook-Air.local>
2016-07-28 16:50:12 +0700

    first commit

---
+++ MapSearchDemo/LocationSearchTable.swift
@@ -0,0 +1,9 @@
+//
+//  LocationSearchTable.swift
+//  MapSearchDemo
+//
+//  Created by PT. Wavin Tunas Utama on 7/26/16.
+//  Copyright © 2016 PT. Wavin Tunas Utama. All rights reserved.
+//
+
+import Foundation
