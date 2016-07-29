f5129608c84472874df3d89d23d04ee024dc6907
PT. Wavin Tunas Utama <ICT@PTs-MacBook-Air.local>
2016-07-29 10:28:28 +0700

    map and uisearchcontroller

--- MapSearchDemo/LocationSearchTable.swift
+++ MapSearchDemo/LocationSearchTable.swift
@@ -6,4 +6,67 @@
 //  Copyright © 2016 PT. Wavin Tunas Utama. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SwiftyJSON
+
+class LocationSearchTable: UITableViewController {
+    
+    var custData: JSON = []
+    
+    let data = ["New York, NY", "Los Angeles, CA", "Chicago, IL", "Houston, TX",
+                "Philadelphia, PA", "Phoenix, AZ", "San Diego, CA", "San Antonio, TX",
+                "Dallas, TX", "Detroit, MI", "San Jose, CA", "Indianapolis, IN",
+                "Jacksonville, FL", "San Francisco, CA", "Columbus, OH", "Austin, TX",
+                "Memphis, TN", "Baltimore, MD", "Charlotte, ND", "Fort Worth, TX"]
+    
+    var filteredData: [String]!
+    
+    func getCustData() {
+        let path = NSBundle.mainBundle().pathForResource("cust_toko", ofType: "json")
+        let jsonData = NSData(contentsOfFile: path!)
+        let json = JSON(data: jsonData!)
+        
+        self.custData = json
+        //self.collectionView.reloadData()
+        
+        print("JSON: \(json)")
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredData.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell")!
+        cell.textLabel?.text = filteredData[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print(filteredData[indexPath.row])
+    }
+}
+
+extension LocationSearchTable: UISearchResultsUpdating {
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text {
+            filteredData = searchText.isEmpty ? data : data.filter({(dataString: String) -> Bool in
+                return dataString.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil
+            })
+            
+            tableView.reloadData()
+        }
+    }
+}
+/*
+extension LocationSearchTable {
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell")!
+        cell.textLabel?.text = filteredData[indexPath.row]
+        return cell
+    }
+}*/
--- MapSearchDemo/ViewController.swift
+++ MapSearchDemo/ViewController.swift
@@ -7,13 +7,50 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var searchController: UISearchController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let locationSearchTable = storyboard!.instantiateViewControllerWithIdentifier("LocationSearchTable") as! LocationSearchTable
+        
+        locationSearchTable.getCustData()
+        locationSearchTable.tableView.dataSource = locationSearchTable.self
+        locationSearchTable.filteredData = locationSearchTable.data
+        
+        searchController = UISearchController(searchResultsController: locationSearchTable)
+        searchController?.searchResultsUpdater = locationSearchTable.self
+        
+        //searchController.dimsBackgroundDuringPresentation = false
+        
+        searchController.searchBar.sizeToFit()
+        
+        let searchBar = searchController!.searchBar
+        //searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        navigationItem.titleView = searchController?.searchBar
+        
+        searchController?.hidesNavigationBarDuringPresentation = false
+        searchController?.dimsBackgroundDuringPresentation = false
+        
+        definesPresentationContext = true
     }
+    /*
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text {
+            filteredData = searchText.isEmpty ? data : data.filter({(dataString: String) -> Bool in
+                return dataString.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil
+            })
+            
+            tableView.reloadData()
+        }
+    }*/
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
